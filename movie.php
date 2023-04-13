1cca04295c0ac96fb306207b19b0c285f528c3b0
KuroBerry <103157666+KuroBerry@users.noreply.github.com>
2023-04-14 00:00:58 +0700

    .

---
+++ movie.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercise 1</title>
+
+    <link rel="stylesheet" href="./PHP/index.php" />
+    <link rel="stylesheet" href="./CSS/index.css" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    />
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+
+  <body style="background-color: rgb(20, 20, 20)">
+    <div class="tool-bar">sdfsdfHI</div>
+    
+    <div class="container">
+      <div class="display col-sm-6 col-md-4 col-lg-3 col-xl-3 mb-3">
+        <img src="avatar.png" alt="">
+      </div>
+    </div>
+
+    <footer>Hiii</footer>
+  </body>
+</html>
